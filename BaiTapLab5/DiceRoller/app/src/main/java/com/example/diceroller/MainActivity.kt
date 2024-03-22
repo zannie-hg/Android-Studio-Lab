f7a162eb573266a26180bad691b3cd15b96c02e5
zannie-hg <hagiangnguyen1903@gmail.com>
2024-03-22 15:10:38 +0700

    update

--- BaiTapLab5/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ BaiTapLab5/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,4 +1,3 @@
-
 package com.example.diceroller
 
 import android.os.Bundle
@@ -6,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -24,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -42,35 +38,55 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Preview
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
-}
-
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf( 1) }
-    val imageResource = when(result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
     }
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(imageResource), contentDescription = result.toString())
 
-        Button(
-            onClick = { result = (1..6).random() },
+    @Preview
+    @Composable
+    fun DiceRollerApp() {
+        DiceWithButtonAndImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
+    }
+
+    @Composable
+    fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+        var result by remember { mutableStateOf(1) }
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = result.toString()
+            )
+            Button(onClick = { result = (1..6).random() })
+            {
+                Text(text = stringResource(R.string.roll), fontSize = 24.sp)
+            }
         }
     }
 }
+
+
+
+
+
+
+
